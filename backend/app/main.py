5a007e8d926aaccba53a11bec81c8f6cf9027ab4
Abhinav3457 <abhinav2005thakur@gmail.com>
2026-04-14 14:39:34 +0530

    udated

--- backend/app/main.py
+++ backend/app/main.py
@@ -42,6 +42,7 @@ app.add_middleware(WAFMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=_build_cors_origins(),
+    allow_origin_regex=r"^http://(localhost|127\.0\.0\.1)(:\d+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
